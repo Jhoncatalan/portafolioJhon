57facfeda1b9e1704ca3639147c2c655fb2b47d4
Johnayfer <jcatalanmoneris@gmail.com>
2023-02-09 08:18:15 -0500

    subiendo mi portafolio

---
+++ texto-animado.js
@@ -0,0 +1,8 @@
+/* ---------evento texto-aleatorio ----------- */
+
+const typed = new Typed(".texto-aleatorio", {
+    strings: ["Programador Web Full Stack Junior"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+});
